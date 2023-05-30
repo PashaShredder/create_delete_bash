525dc783d47ceafdec4bda98550fb5f2e8ab441b
Shesterikov-Pavel <105942922+PashaShredder@users.noreply.github.com>
2023-05-30 19:32:08 +0300

    Update folder.sh

--- folder.sh
+++ folder.sh
@@ -1,34 +1,18 @@
-#!/bin/bash  -  первая строчка “шебанг” означает что скрипт будет выполняться в оболочке “bash”
-
-# Указываем к-во папок для создания и путь к директории куда они должны сохраниться 
-
+#!/bin/bash  
 
 folders_count=10
 
 path_to_save=/home/ubuntu/folder_script/
 
-# Получаем текущую дату
 
 current_date=$(date +%Y.%m.%d)
 
-# Создаем цикл для создания папок
 
 for ((i=1;i<=$folders_count;i++)); do
-
-  # Формируем название папки
-
-  folder_name="${current_date}_Папка$i"
-
-  # Создаем папку с заданным названием в директории 
+ 
+ folder_name="${current_date}_Папка$i"
 
   mkdir -p "$path_to_save/$folder_name"
 done
 
-# Сохранение результатов работы скрипта в лог файл
-
-echo "Скрипт завершен в $(date)" >> $path_to_save/script.log
-
-
-
-
-# ./folder.sh -  запуск скрипта 
+echo "Script finished in $(date)" >> $path_to_save/script.log
